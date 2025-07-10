cec947975b4207e814870a41768563a8eda51255
CarlosVSL <carlosvsanchezlopez@gmail.com>
2025-07-10 20:40:18 +0200

    Fix map detection

--- src/parse/parse_utils.c
+++ src/parse/parse_utils.c
@@ -1,42 +1,42 @@
-#include "../../include/cub3d.h"
+#include "cub3d.h"
 
 /* -------------------------------------------------------------------------- */
-/*	Trim leading spaces/tabs                                                  */
+/*  Trim leading spaces/tabs                                                  */
 /* -------------------------------------------------------------------------- */
-char	*skip_spaces(char *s)
+char    *skip_spaces(char *s)
 {
-	while (*s == ' ' || *s == '\t')
-		++s;
-	return (s);
+    while (*s == ' ' || *s == '\t')
+        ++s;
+    return (s);
 }
 
 /* -------------------------------------------------------------------------- */
-/*	Read an integer, advance ptr, store in out                                */
+/*  Read an integer, advance ptr, store in out                                */
 /* -------------------------------------------------------------------------- */
-int	parse_int(const char **str, int *out)
+int     parse_int(const char **str, int *out)
 {
-	int	val;
+    int val;
 
-	val = 0;
-	*str = skip_spaces((char *)*str);
-	if (**str < '0' || **str > '9')
-		return (-1);
-	while (**str >= '0' && **str <= '9')
-	{
-		val = val * 10 + (**str - '0');
-		++(*str);
-	}
-	*out = val;
-	return (0);
+    val = 0;
+    *str = skip_spaces((char *)*str);
+    if (**str < '0' || **str > '9')
+        return (-1);
+    while (**str >= '0' && **str <= '9')
+    {
+        val = val * 10 + (**str - '0');
+        ++(*str);
+    }
+    *out = val;
+    return (0);
 }
 
 /* -------------------------------------------------------------------------- */
-/*	Detect whether a line belongs to the map                                  */
+/*  Detect whether a line belongs to the map                                  */
+/*  Only lines that start with '1' (after any spaces) are considered map      */
 /* -------------------------------------------------------------------------- */
-int	is_map_line(const char *s)
+int     is_map_line(const char *s)
 {
-	while (*s == ' ')
-		++s;
-	return (*s == '1' || *s == '0' || *s == 'N' \
-		|| *s == 'S' || *s == 'E' || *s == 'W');
+    while (*s == ' ' || *s == '\t')
+        ++s;
+    return (*s == '1');
 }
